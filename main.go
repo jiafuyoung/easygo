e7b822a90471c86a31797bcdb50158164d2bb019
jiafuyoung <jiafu_young@163.com>
2024-10-24 14:15:49 +0800

    测试版本问题

--- main.go
+++ main.go
@@ -14,4 +14,5 @@ func main() {
 	fmt.Println("Top element:", top, "OK:", ok)      // 输出: Top element: 2 OK: true
 	stack.Pop()                                      // 出栈操作但不获取元素值，仅检查是否成功移除顶部元素
 	fmt.Println("Stack is empty:", !stack.IsEmpty()) // 检查栈是否为空并输出结果: Stack is empty: false
+	fmt.Println("版本怎么弄啊")
 }
